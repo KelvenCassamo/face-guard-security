7e5d39a05266bbeed868b9877635b4b7600368a6
Kelven Cassamo <86805513+KelvenCassamo@users.noreply.github.com>
2025-10-24 23:40:39 +0200

    Add welcome message

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+from modules.registration import register_face
 from modules.recognition import recognize_face, recognize_all_faces, recognize_faces_in_real_time, \
     recognize_theft_faces_in_real_time
-from modules.registration import register_face
 
 
 def main():
-
     while True:
+        print("\n--- BEM-VINDO AO FACE GUARD SECURITY ---")
         print("\n--- MENU ---")
-        print("1. Registrar rosto")
+        print("1. Registar rosto")
         print("2. Reconhecer rosto")
         print("3. Reconhecer rostos")
         print("4. Reconhecimento de rostos em tempo real")
